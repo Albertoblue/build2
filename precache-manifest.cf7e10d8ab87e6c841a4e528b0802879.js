bf9df55c28b8d91cbb5399e9f068e63c96f9eb02
Alberto Alcantara <jalcantara@vsys.com>
2022-02-18 21:54:20 -0600

    Primer commit

---
+++ precache-manifest.cf7e10d8ab87e6c841a4e528b0802879.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0cec443406a4e57f1231d0de75b4b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67f59b46b8269a87b09b",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "85149c33343d32aff70f",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "67f59b46b8269a87b09b",
+    "url": "/static/js/2.6175962a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6175962a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85149c33343d32aff70f",
+    "url": "/static/js/main.3d17b38a.chunk.js"
+  },
+  {
+    "revision": "0eca4f07d3188be3b79d",
+    "url": "/static/js/runtime-main.2a38a0dd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
